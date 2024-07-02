14ed911e7721521de53f846b1f73e396f95dfbce
Emilio Chang <71205903+EmilioChang@users.noreply.github.com>
2024-07-02 15:11:45 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 # Run from Chess (root) directory
-python3 -m Game.GUI.Game
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    if uname -a | grep -qi "MINGW"; then
+        python -m Game.GUI.Game
+    else
+        python3 -m Game.GUI.Game
+    fi
+fi
