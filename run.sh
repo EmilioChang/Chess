bd57f6912296670a25f6d425de331a19d7e8f601
Emilio Chang <71205903+EmilioChang@users.noreply.github.com>
2024-07-02 15:26:27 -0600

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-# Run from Chess (root) directory
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    if uname -a | grep -qi "MINGW"; then
-        python -m Game.GUI.Game
-    else
-        python3 -m Game.GUI.Game
-    fi
-fi
